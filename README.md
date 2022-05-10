51400932313beda31c435cc9c14666f8cb78146e
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2022-05-10 16:39:36 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# `cargo-spdx`
+
+`cargo-spdx` provides a cargo subcommand to generate an
+[SPDX][spdx] Software Bill of Materials (SBOM) for a Rust crate.
+
+## Status
+
+`cargo-spdx` is currently in development and not yet ready for use.
+
+## License
+
+`cargo-spdx` is dual-licensed with the MIT or Apache 2.0 licenses.
+
+[spdx]: https://spdx.dev
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+/**
+ * Basically, this tool should work as follows:
+ *
+ * Find the root of the crate.
+ * Use cargo metadata to get the dependency graph and crate info.
+ * Put that info into SPDX format.
+ */
+use anyhow::{anyhow, Result};
+use cargo_metadata::{Metadata, MetadataCommand, Package};
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("error: {}", e);
+    }
+}
+
+fn run() -> Result<()> {
+    // Get the metadata for the crate.
+    let metadata = MetadataCommand::new().exec()?;
+
+    // Get the root of the dependency tree.
+    let root = get_root(&metadata).ok_or_else(|| anyhow!("no root"))?;
+    println!("root: {}", root.name);
+
+    Ok(())
+}
+
+fn get_root(metadata: &Metadata) -> Option<&Package> {
+    metadata
+        .resolve
+        .as_ref()
+        .and_then(|r| r.root.as_ref().map(|r| &metadata[r]))
+}
